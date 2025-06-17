5abbe6b2d52c86183d04f825a7b1e5d11e133567
Mohammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2025-06-17 11:34:03 +0530

    d

--- Config/Connection.js
+++ Config/Connection.js
@@ -1,3 +1,35 @@
+// //live db
+// import mysql from "mysql2";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const dbConfig = {
+//   host: "shinkansen.proxy.rlwy.net",
+//   port: 36724,
+//   user: "root",
+//   password: "knXfjVtnqprCIfozUaLymhPfJuNSfwoR",
+//   database: "railway",
+//   charset: "utf8mb4",
+//   multipleStatements: false,
+//   timezone: "Z",
+// };
+
+
+// const pool = mysql.createPool(dbConfig);
+
+// pool.promise()
+//   .getConnection()
+//   .then((connection) => {
+//     console.log("Database connected successfully");
+//     connection.release(); 
+//   })
+//   .catch((err) => {
+//     console.error("Error connecting to the database:", err);
+//   });
+
+// export default pool.promise();
+
 //live db
 import mysql from "mysql2";
 import dotenv from "dotenv";
@@ -5,11 +37,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const dbConfig = {
-  host: "shinkansen.proxy.rlwy.net",
-  port: 36724,
+  host: "localhost",
+  port: 3306,
   user: "root",
-  password: "knXfjVtnqprCIfozUaLymhPfJuNSfwoR",
-  database: "railway",
+  password: "",
+  database: "crm",
   charset: "utf8mb4",
   multipleStatements: false,
   timezone: "Z",
