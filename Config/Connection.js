942dd21fa0385dd701464970770d45935dbebd6a
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-27 15:50:11 +0530

    Update Connection.js

--- Config/Connection.js
+++ Config/Connection.js
@@ -5,11 +5,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const dbConfig = {
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "netaaicrm",
+  host: "trolley.proxy.rlwy.net",        // ✅ Updated Host
+  port: 12653,                            // ✅ Updated Port
+  user: "root",                           // ✅ Updated User
+  password: "hGRAtSzjpWhYFefQymlhiFUphnrSAumj",  // ✅ Updated Password
+  database: "railway",                   // ✅ Updated Database
   charset: "utf8mb4",
   multipleStatements: false,
   timezone: "Z",
@@ -29,36 +29,4 @@ pool.promise()
   });
 
 export default pool.promise();
-
-//live db
-// import mysql from "mysql2";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const dbConfig = {
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "",
-//   database: "crm",
-//   charset: "utf8mb4",
-//   multipleStatements: false,
-//   timezone: "Z",
-// };
-
-
-// const pool = mysql.createPool(dbConfig);
-
-// pool.promise()
-//   .getConnection()
-//   .then((connection) => {
-//     console.log("Database connected successfully");
-//     connection.release(); 
-//   })
-//   .catch((err) => {
-//     console.error("Error connecting to the database:", err);
-//   });
-
-// export default pool.promise();
 
