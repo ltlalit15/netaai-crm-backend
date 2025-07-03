c8a8ae8e919b9334d9a19fee119e327f0955d697
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-07-03 16:19:21 +0530

    Create DocumentRecordRouter.js

---
+++ Routers/DocumentRecordRouter.js
@@ -0,0 +1,28 @@
+import express from "express";
+import DocumentRecordController from "../Controllers/DocumentRecordCtrl.js";
+
+const router = express.Router();
+
+// Create a document
+router.post("/documentsRecord", DocumentRecordController.createDocumentRecord);
+
+// Get all documents
+router.get("/documentsRecord", DocumentRecordController.getAllDocumentsRecord);
+
+// Get a document by ID
+router.get("/documentsRecord/:id", DocumentRecordController.getDocumentByIdRecord);
+
+// Update a document
+router.patch("/documentsRecord/:id", DocumentRecordController.updateDocumentRecord);
+
+// Delete a document
+router.delete("/documentsRecord/:id", DocumentRecordController.deleteDocumentRecord);
+
+router.get("/documentsRecord/getDocumentsByProposalId/:proposal_id", DocumentRecordController.getDocumentsByProposalId);
+
+export default router;
+
+
+
+
+
